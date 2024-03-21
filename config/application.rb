2f70643c558d25e9c007641a8bec829ff245157c
Killua7362 <bhat7362@gmail.com>
2024-03-21 17:43:48 +0530

    back to api mode

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,12 @@ module Api
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.session_store :cookie_store, key: '_interslice_session', domain: "blogger-v2.onrender.com", same_site: :none
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+  
+    config.action_dispatch.cookies_same_site_production =
+      :none
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -26,6 +32,6 @@ module Api
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = false
+    config.api_only = true
   end
 end
--- config/initializers/session_store.rb
+++
@@ -1,2 +0,0 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "blogger-v2.onrender.com", same_site: :none
-# Api::Application.config.session_store :cookie_store, :key => "_authentication_app", :domain => :all, :same_site => :none
